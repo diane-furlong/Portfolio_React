14562498215f07ced147ffc4a23c27ac934b9566
diane-furlong <dfurlongm@gmail.com>
2021-04-18 00:37:19 -0400

    added particles and typed npm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Particles from "react-particles-js"
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Particles
+        params={{
+          particles: {
+            line_linked: {
+              enable: false
+            },
+            number: {
+              value: 90,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 4,
+                color: "#f5b183"
+              }
+            }
+          }
+        }}
+      /> 
       <Navbar/>
-      <h2>Portfolio!</h2>
-    </div>
+      <Header/>
+      <h2>portfolio</h2>
+    </>
   );
 }
 
