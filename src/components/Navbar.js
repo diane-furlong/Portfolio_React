b0285acc6d960e2ac1b17377b872b6a45b519c8f
diane-furlong <dfurlongm@gmail.com>
2021-04-20 22:57:17 -0400

    routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const navbar = () => {
     return (
       <nav className="navbar navbar-expand-lg fixed-top">
         <div className="container nav-item">
-          <a className="navbar-brand" href="/Portfolio_React" id="logo">Diane Furlong</a>
+          <a className="navbar-brand" href="/Portfolio_React/#/" id="logo">Diane Furlong</a>
 
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
@@ -29,16 +29,16 @@ const navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="/Portfolio_React">Home <span className="sr-only">(current)</span></a>
+                <a className="nav-link" href="/Portfolio_React/#/">Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/Portfolio_React/AboutMe/">About Me</a>
+                <a className="nav-link" href="/Portfolio_React/#/AboutMe/">About Me</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/Portfolio_React/Portfolio/">Portfolio</a>
+                <a className="nav-link" href="/Portfolio_React/#/Portfolio/">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/Portfolio_React/Contact/">Contact</a>
+                <a className="nav-link" href="/Portfolio_React/#/Contact/">Contact</a>
               </li>
             </ul>
           </div>
