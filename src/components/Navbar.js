67aa86daf6eac2beb983c776b38c1bfe9abdc5e4
diane-furlong <dfurlongm@gmail.com>
2021-04-20 23:13:56 -0400

    routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,13 +32,13 @@ const navbar = () => {
                 <a className="nav-link" href="/Portfolio_React/#/">Home <span className="sr-only">(current)</span></a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#/AboutMe/">About Me</a>
+                <a className="nav-link" href="/Portfolio_React/#/AboutMe/">About Me</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#/Portfolio/">Portfolio</a>
+                <a className="nav-link" href="/Portfolio_React/#/Portfolio/">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/#/Contact/">Contact</a>
+                <a className="nav-link" href="/Portfolio_React/#/Contact/">Contact</a>
               </li>
             </ul>
           </div>
