924307a35a33c0ccec851b622c4adb4b9bc940a5
diane-furlong <dfurlongm@gmail.com>
2021-04-20 23:11:20 -0400

    routing again

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Navbar from './components/Navbar'
 import Header from './components/Header'
-import Portfolio from "./components/Portfolio"
-import AboutMe from "./components/AboutMe"
-import Contact from "./components/Contact"
+import Portfolio from './components/Portfolio'
+import AboutMe from './components/AboutMe'
+import Contact from './components/Contact'
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
       <Navbar/>
       <HashRouter basename="/">
         <Route exact path="/" component={ Header }/>
-        <Route path="/#/AboutMe/" component={ AboutMe }/>
-        <Route path="/#/Portfolio/" component={ Portfolio }/>
-        <Route path="/#/Contact/" component={ Contact }/>
+        <Route exact path="/#/AboutMe/" component={ AboutMe }/>
+        <Route exact path="/#/Portfolio/" component={ Portfolio }/>
+        <Route exact path="/#/Contact/" component={ Contact }/>
       </HashRouter>
     </>
   );
